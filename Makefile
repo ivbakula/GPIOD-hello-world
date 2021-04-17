333d48c8439ec9d7875af690669855d2212e547f
Ivan Bakula <i.bakula@yahoo.com>
2021-04-17 14:11:48 +0200

    Works with gpio-fan.dtbo

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 ifneq ($(KERNELRELEASE),)
-	obj-m := <>.o
+	obj-m := gpio.o
 
 else
 	KERNELDIR ?= /lib/modules/$(shell uname -r)/build
 	PWD := $(shell pwd)
-
+	OVERLAY=hello_world
+	OVERLAY_SRC=$(OVERLAY).dts
+	OVERLAY_BIN=$(OVERLAY).dtbo
 default:
 	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules
 
 clean:
 	$(MAKE) -C $(KERNELDIR) M=$(PWD) clean
+
+overlay:
+	dtc -@ -I dts -O dtb -o $(OVERLAY_BIN) $(OVERLAY_SRC)
+	
 endif
--- gpio.c
+++ gpio.c
@@ -1,11 +1,35 @@
+#include <linux/err.h>
+#include <linux/gpio/driver.h>
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/gpio/machine.h>
-#include <linux/gpio/consumer.h>
+#include <linux/platform_device.h>
+#include <soc/bcm2835/raspberrypi-firmware.h>
 
-struct gpiod_lookup_table gpios_table = {
-	.dev_id = "leds.0",
-	.table = {
-		GPIO_LOOKUP_IDX(GPI016, UINT16_MAX)
-	},
+#define MODULE_NAME "hello"
+static int gpio_probe(struct platform_device *pdev)
+{
+	printk(KERN_ALERT "gpio_probe.\n");
+	return 0;
 }
+
+static int gpio_remove(struct platform_device *pdev)
+{
+	printk(KERN_ALERT "gpio_remove.\n");
+	return 0;
+}
+
+static const struct of_device_id gpio_ids[] = {
+	{ .compatible = "gpio-fan"},
+	{ }
+};
+MODULE_DEVICE_TABLE(of, gpio_ids);
+
+static struct platform_driver gpio_driver = {
+	.driver = {
+		.name = MODULE_NAME,
+		.of_match_table = of_match_ptr(gpio_ids),
+	},
+	.probe = gpio_probe,
+	.remove = gpio_remove
+};
+module_platform_driver(gpio_driver);
+MODULE_LICENSE("GPL");
